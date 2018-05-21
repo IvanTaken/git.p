e8eb5c3daff925b9b6d32636f3d8a85f10f020ec
ognib <39418949+ognib@users.noreply.github.com>
2018-05-21 18:27:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,3 +8,10 @@ int rec;
 double itteration(double,double*,int);
 
 double recursion(double,double*,int);
+
+double recursion(double x, double *arr, int n)
+{
+    rec++;
+    if ( n > 1 ) return arr[n] + recursion( x, arr, n-1 ) * x;
+    return arr[0] * x + arr[1];
+}
